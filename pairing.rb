f96706805e28dd74c2654bfd4e6c1232804dc2fc
vnframe <vnframe95@icloud.com>
2017-07-19 12:37:58 -0400

    donnnne

--- pairing.rb
+++ pairing.rb
@@ -2,5 +2,11 @@ def pairs(names)
 
   pair = names.shuffle.each_slice(2).to_a
   pair
-
+  if names.length % 2 == 0
+  else
+    last = pair.pop
+    last = last[0]
+    pair[0] << last
+  end
+pair
 end
