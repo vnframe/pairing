e88243f1059f0af36573cc9ec4956c906236f066
vnframe <vnframe95@icloud.com>
2017-07-21 14:30:43 -0400

    ughhh

--- pairing.rb
+++ pairing.rb
@@ -1,11 +1,27 @@
 def pairs(names)
-  pair = names.shuffle.combination(2).to_a
+
+  pair = names.shuffle.combination(1).to_a
   pair = pair.uniq { |s| s.first }
-  if names.length % 2 == 0
-  else
-    last = pair.pop
-    last = last[0]
-    pair[0] << last
+end
+def pair_num(names)
+  pair = pairs(names)
+  if pair.length % 2 == 1
+    extra = pair.pop
+    extra = extra[0]
   end
- p pair
+  array = []
+  numpairs = names.length/2
+  p numpairs
+ numpairs.times do
+    array2 = []
+    last = pair.pop
+    last2 = pair.pop
+    last = last[0]
+    last2 = last2[0]
+    array2 << last
+    array2 << last2
+    array << array2
+  end
+  array[0] << extra
+ array
 end
--- pairing_test.rb
+++ pairing_test.rb
@@ -11,14 +11,14 @@ class Test_Paring < Minitest::Test
   #   results = pairs(names)
   #   assert_equal(2, results.count)
   # end
-  # def test_multiple_pairs
-  #   names = ["Katie", "Derick", "Steph", "Ty", "Chris", "Robert", "Marcus", "Aaron", "Derrick G.", "Andrew", "Adrian", "John", "Steven", "Tori", "Tyler", "Mike"]
-  #   results = pairs(names)
-  #   assert_equal(8, results.count)
-  # end
+  def test_multiple_pairs
+    names = ["Katie", "Derick", "Steph", "Ty", "Chris", "Robert", "Marcus", "Aaron", "Derrick G.", "Andrew", "Adrian", "John", "Steven", "Tori", "Tyler", "Mike"]
+    results = pairs(names)
+    assert_equal(8, results.count)
+  end
   def test_with_odd
      names = ["Max", "Katie", "Derick", "Steph", "Ty", "Chris", "Robert", "Marcus", "Aaron", "Derrick G.", "Andrew", "Adrian", "John", "Steven", "Tori", "Tyler", "Mike"]
-     results = pairs(names)
+    p results = pair_num(names)
     p assert_equal(8, results.count)
   end
 end
