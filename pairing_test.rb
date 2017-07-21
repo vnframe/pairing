4a9878188ba8fcbc0e4fae37afc819794fe43225
vnframe <vnframe95@icloud.com>
2017-07-21 14:38:13 -0400

    done

--- pairing_test.rb
+++ pairing_test.rb
@@ -6,14 +6,14 @@ class Test_Paring < Minitest::Test
   #   names = ["Katie", "Derick", "Steph", "Ty"]
   #   assert_equal([["Katie", "Derick"], ["Steph", "Ty"]], pairs(names))
   # end
-  # def test_random_pairs
-  #   names = ["Katie", "Derick", "Steph", "Ty"]
-  #   results = pairs(names)
-  #   assert_equal(2, results.count)
-  # end
+  def test_random_pairs
+    names = ["Katie", "Derick", "Steph", "Ty"]
+    results = pair_num(names)
+    assert_equal(2, results.count)
+  end
   def test_multiple_pairs
     names = ["Katie", "Derick", "Steph", "Ty", "Chris", "Robert", "Marcus", "Aaron", "Derrick G.", "Andrew", "Adrian", "John", "Steven", "Tori", "Tyler", "Mike"]
-    results = pairs(names)
+    results = pair_num(names)
     assert_equal(8, results.count)
   end
   def test_with_odd
